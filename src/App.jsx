cac45219a0cc4c2005d7f0c2427fa41c6f8889c2
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-07-24 15:04:16 +0500

    first commit - project started

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-orange-600 font-bold flex items-center justify-center">
+        Realtor website which is a real estate website.
+      </h1>
+    </>
+  );
+}
+
+export default App;
