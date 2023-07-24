c3a60ae8e20c09a158e3fd8f1985871111d1858f
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-07-24 15:31:08 +0500

    created pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import { Outlet } from "react-router-dom";
+
 function App() {
   return (
     <>
-      <h1 className="text-orange-600 font-bold flex items-center justify-center">
-        Realtor website which is a real estate website.
-      </h1>
+      <Outlet />
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import Home from "./pages/Home.jsx";
+import Profile from "./pages/Profile.jsx";
+import SignIn from "./pages/SignIn.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import ForgotPassword from "./pages/ForgotPassword.jsx";
+import Offer from "./pages/Offer.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
+      },
+      {
+        path: "/sign-in",
+        element: <SignIn />,
+      },
+      {
+        path: "/sign-up",
+        element: <SignUp />,
+      },
+      {
+        path: "/forgot-password",
+        element: <ForgotPassword />,
+      },
+      {
+        path: "/offer",
+        element: <Offer />,
+      },
+    ],
+  },
+]);
+
+const root = createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={router} />);
