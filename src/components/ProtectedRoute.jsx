f44367074e2c68ece06d5e369804a6515a31f1ac
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-07-30 12:25:54 +0500

    create the spinner component

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,14 +1,14 @@
 import { Navigate } from "react-router-dom";
 import useAuthStatus from "../hooks/useAuthStatus";
 import Profile from "../pages/Profile";
+import Spinner from "./Spinner";
 
 const ProtectedRoute = () => {
   const { loggedIn, checkingStatus } = useAuthStatus();
 
-  if (checkingStatus) return <h1>Loading...</h1>;
+  if (checkingStatus) return <Spinner />;
 
-  return loggedIn ? <Profile /> :  <Navigate to="/sign-in" />;
-  
+  return loggedIn ? <Profile /> : <Navigate to="/sign-in" />;
 };
 
 export default ProtectedRoute;
