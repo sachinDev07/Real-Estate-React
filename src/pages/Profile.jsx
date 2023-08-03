69306338fe657979af301597627e85fd70ded056
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-08-03 13:18:24 +0500

    add my listings section to the profile page and create the listingItem component

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { auth, db } from "../config/firebase";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { updateProfile } from "firebase/auth";
-import { doc, updateDoc } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  orderBy,
+  query,
+  updateDoc,
+  where,
+} from "firebase/firestore";
 import { FcHome } from "react-icons/fc";
+import ListingItem from "../components/ListingItem";
+
 const Profile = () => {
   const [changeDetails, setChangeDetails] = useState(false);
+  const [listings, setListings] = useState([]);
+  const [loading, setLoading] = useState(true);
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
@@ -47,58 +60,105 @@ const Profile = () => {
     }
   };
 
+  const fetchUserListings = async () => {
+    const listingRef = collection(db, "listings");
+    const q = query(
+      listingRef,
+      where("userRef", "==", auth.currentUser.uid),
+      orderBy("timestamp", "desc")
+    );
+    const querySnap = await getDocs(q);
+    let listings = [];
+    querySnap.forEach((doc) => {
+      return listings.push({
+        id: doc.id,
+        data: doc.data(),
+      });
+    });
+    setListings(listings);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchUserListings();
+  }, []);
+
   return (
-    <section className="max-w-6xl mx-auto flex flex-col justify-center items-center">
-      <h1 className="text-center text-3xl mt-6 font-bold">My Profile</h1>
-      <div className="w-full md:w-[50%] mt-6 px-3">
-        <form>
-          <input
-            type="text"
-            value={name}
-            id="name"
-            disabled={!changeDetails}
-            onChange={onChange}
-            className={`mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out ${
-              changeDetails && "bg-red-200 focus:bg-red-100"
-            }`}
-          />
-          <input
-            type="email"
-            value={email}
-            id="email"
-            disabled={!changeDetails}
-            onChange={onChange}
-            className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out"
-          />
-          <div className="flex justify-between items-center whitespace-nowrap text-sm sm:text-lg">
-            <p>
-              Do you want to change your name?
-              <span
-                className="text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer"
-                onClick={() => {
-                  changeDetails && onSubmit();
-                  setChangeDetails((prevState) => !prevState);
-                }}
+    <>
+      <section className="max-w-6xl mx-auto flex flex-col justify-center items-center">
+        <h1 className="text-center text-3xl mt-6 font-bold">My Profile</h1>
+        <div className="w-full md:w-[50%] mt-6 px-3">
+          <form>
+            <input
+              type="text"
+              value={name}
+              id="name"
+              disabled={!changeDetails}
+              onChange={onChange}
+              className={`mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out ${
+                changeDetails && "bg-red-200 focus:bg-red-100"
+              }`}
+            />
+            <input
+              type="email"
+              value={email}
+              id="email"
+              disabled={!changeDetails}
+              onChange={onChange}
+              className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out"
+            />
+            <div className="flex justify-between items-center whitespace-nowrap text-sm sm:text-lg">
+              <p>
+                Do you want to change your name?
+                <span
+                  className="text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer"
+                  onClick={() => {
+                    changeDetails && onSubmit();
+                    setChangeDetails((prevState) => !prevState);
+                  }}
+                >
+                  {changeDetails ? "Apply changes" : "Edit"}
+                </span>
+              </p>
+              <p
+                className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out cursor-pointer"
+                onClick={onLogout}
               >
-                {changeDetails ? "Apply changes" : "Edit"}
-              </span>
-            </p>
-            <p
-              className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out cursor-pointer"
-              onClick={onLogout}
+                Sign out
+              </p>
+            </div>
+          </form>
+          <button
+            type="submit"
+            className="mt-6 w-full bg-blue-600 text-white uppercase px-7 py-3 text-sm font-medium rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800"
+          >
+            <Link
+              to="/create-listing"
+              className="flex items-center justify-center"
             >
-              Sign out
-            </p>
-          </div>
-        </form>
-        <button type="submit" className="mt-6 w-full bg-blue-600 text-white uppercase px-7 py-3 text-sm font-medium rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800">
-          <Link to="/create-listing" className="flex items-center justify-center">
-            <FcHome className="mr-2 text-3xl bg-red-200 rounded-full p-1 border-2" />
-            Sell or rent your home
-          </Link>
-        </button>
+              <FcHome className="mr-2 text-3xl bg-red-200 rounded-full p-1 border-2" />
+              Sell or rent your home
+            </Link>
+          </button>
+        </div>
+      </section>
+      <div className="max-w-6xl px-3 mt-6 mx-auto">
+        {!loading && listings.length > 0 && (
+          <>
+            <h2 className="text-2xl text-center font-semibold">My Listings</h2>
+            <ul>
+              {listings.map((listing) => (
+                <ListingItem
+                  key={listing.id}
+                  id={listing.id}
+                  listing={listing.data}
+                />
+              ))}
+            </ul>
+          </>
+        )}
       </div>
-    </section>
+    </>
   );
 };
 
