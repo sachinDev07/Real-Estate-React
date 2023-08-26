bbbf1b78ca68212b17a1cc2e06a5606e3bd8497c
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-08-26 18:49:05 +0500

    add map to the listing page using leaflet and react-leaflet

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -20,6 +20,8 @@ import { EffectFade, Autoplay, Navigation, Pagination } from "swiper/modules";
 import "swiper/css/bundle";
 import "swiper/css/navigation";
 
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+
 const Listing = () => {
   const params = useParams();
   const [listing, setListing] = useState(null);
@@ -148,8 +150,23 @@ const Listing = () => {
             <Contact userRef={listing.userRef} listing={listing} />
           )}
         </div>
-        <div className="w-full h-[200px] md:h-[400px] z-10 overflow-x-hidden">
-
+        <div className="w-full h-[200px] md:h-[400px] z-10 overflow-x-hidden mt-6f md:mt-0 md:ml-2">
+          <MapContainer
+            // center={[listing.geolocation.lat, listing.geolocation.lng]}
+            zoom={13}
+            scrollWheelZoom={false}
+            style={{ height: "100%", width: "100%" }}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker
+              // position={[listing.geolocation.lat, listing.geolocation.lng]}
+            >
+              <Popup>{listing.address}</Popup>
+            </Marker>
+          </MapContainer>
         </div>
       </div>
     </main>
